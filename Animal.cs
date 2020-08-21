2aad9c759db0b69bf9f9fb11c1c247e24a9eaeac
Rijan Chapagain <33564044@student.murdoch.edu.au>
2020-08-21 12:14:22 +0800

    creates abstract class animal

---
+++ Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace AnimalDb
+{
+    class Animal
+    {
+        public Animal(string food, string location, int leg)
+        {
+            _food = food;
+            _location = location;
+            _leg = leg;
+        }
+
+        public Animal()
+        {
+        }
+
+        protected string _food { get; set; }
+        protected int _leg { get; set; }
+
+        protected string _location = "Australia";
+
+        public string animalDetails()
+        {
+            return ("Location is: " + _location + "\nCommon Food is: " + _food + "\nHave " + _leg + " legs\n" );
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AnimalDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kangaroo a = new Kangaroo("meat", "Perth", 4);
+            Kangaroo child = new Kangaroo("meat", "Nepal", 4);
+            Emu e = new Emu("vagetirian", "South Australia", 2);
+
+            Console.WriteLine(a.animalDetails());
+            Console.WriteLine(child.animalDetails());
+            Console.WriteLine(e.animalDetails());
+        }
+    }
+}
