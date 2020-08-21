bba625675e9a5493ded7e8f832cf7b9389afaf73
Rijan Chapagain <33564044@student.murdoch.edu.au>
2020-08-21 12:14:54 +0800

    Adds derived classes

---
+++ Emu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalDb
+{
+    class Emu : Animal
+    {
+        public Emu(string food, string location, int leg) : base(food, location, leg)
+        {
+            
+        }
+    }
+}
---
+++ Kangaroo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalDb
+{
+    class Kangaroo : Animal
+    {
+        public Kangaroo(string food, string location, int leg) : base (food, location, leg)
+        {
+
+        }
+    }
+}
